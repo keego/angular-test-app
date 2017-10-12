fd849aae0a0aa86f2bd0f8ef504afe73a1727571
Keegan Farley <keegomyneego@gmail.com>
2017-10-12 15:21:31 -0700

    ToH pt 2 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'My First Angular App';
+  hero: Hero = {
+    id: 1,
+    name: 'Montecristo',
+  };
 }
