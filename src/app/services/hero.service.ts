bc38914cd6f92d7c58569269a6ba32983ab08284
Keegan Farley <keegomyneego@gmail.com>
2017-10-12 18:22:27 -0700

    finished ToH pt 5

---
+++ src/app/services/hero.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from '$models/hero';
+import { HEROES } from './mocks/mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+}
